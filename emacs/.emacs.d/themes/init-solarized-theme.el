4f56364cca6a4f0fb677045fddae2a0a18097294
pkdahl <pkdahl@gmail.com>
2015-12-14 19:59:29 +0100

    don't increase font height

--- emacs/.emacs.d/themes/init-solarized-theme.el
+++ emacs/.emacs.d/themes/init-solarized-theme.el
@@ -6,7 +6,12 @@
   :init
   (setq solarized-use-variable-pitch nil
 	solarized-scale-org-headlines nil
-	solarized-use-less-bold t)
+	solarized-use-less-bold t
+	;; Do not scale font up
+	solarized-height-plus-1 1.0
+	solarized-height-plus-2 1.0
+	solarized-height-plus-3 1.0
+	solarized-height-plus-4 1.0)
   (load-theme 'solarized-light t))
 
 (provide 'init-solarized-theme)
