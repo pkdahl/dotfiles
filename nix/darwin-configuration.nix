9c248f87770fa7f54d5441aaa79500692ed2f606
Per K. Dahl <pkdahl@gmail.com>
2017-01-07 16:09:28 +0100

    Need nix from nixpkgs

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -2,15 +2,24 @@
 {
   system.defaults.dock.autohide = true;
   system.defaults.dock.orientation = "left";
+  system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
-    [ pkgs.git
+    [ pkgs.nix
+      pkgs.nix-repl
+
+      pkgs.git
       pkgs.gnupg
       pkgs.mosh
-      pkgs.nix-repl
       pkgs.wget
+
+      pkgs.ghc
+      pkgs.cabal2nix
+      pkgs.cabal-install
     ];
 
   # Create /etc/bashrc that loads the nix-darwin environment.
@@ -21,8 +30,7 @@
   services.activate-system.enable = true;
 
   # environment.variables.GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-  environment.variables.SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
+  # environment.variables.SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
   nixpkgs.config.allowUnfree = true;
 }
