2410ec1e43aa1d9e732e0f966e0ce20507ff17b4
Per K. Dahl <pkdahl@gmail.com>
2017-09-04 09:59:19 +0200

    Aliases for UiO courses

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,3 +19,11 @@ alias lsd='ls -ld *(-/DN)' # list only dirs
 # misc
 alias df='df -h'
 alias du='du -h'
+
+# directories
+alias prj='cd ${HOME}/projects'
+alias 1060='cd ${HOME}/projects/uio-inf1060'
+alias 1300='cd ${HOME}/projects/uio-inf1300'
+alias 2220='cd ${HOME}/projects/uio-inf2220'
+alias 3110='cd ${HOME}/projects/uio-inf3110'
+alias 3331='cd ${HOME}/projects/uio-inf3331'
