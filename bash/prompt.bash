4790cc01cf6689f3551283833191a43fd098f9d2
Per Kulseth Dahl <pkdahl@gmail.com>
2017-09-22 10:54:57 +0200

    Remove old Bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,33 +1,4 @@
-set_prompt () {
-Last_Command=$? # Must come first!
-    Blue='\[\e[01;34m\]'
-    White='\[\e[01;37m\]'
-    Red='\[\e[01;31m\]'
-    Green='\[\e[01;32m\]'
-    Reset='\[\e[00m\]'
-    FancyX='\342\234\227'
-    Checkmark='\342\234\223'
-
-    PS1=""
-
-    # If it was successful, print a green check mark.
-    # Otherwise, print a red X.
-    if [[ $Last_Command == 0 ]]; then
-        PS1+="$Green$Checkmark "
-    else
-        PS1+="$Red$FancyX "
-    fi
-    # If root, just print the host in red.
-    # Otherwise, print the current user and host in green.
-    if [[ $EUID == 0 ]]; then
-        PS1+="$Red\\h "
-    else
-        PS1+="$Green\\u@\\h "
-    fi
-    # Print the working directory and prompt marker in blue,
-    # and reset the text color to the default.
-    PS1+="$Blue\\w \\\$$Reset "
-}
+# ~/.config/bash/prompt.bash
 
 set_status() {
     local happy='\[\e[0;32m\]^_^\[\e[0m\]'
@@ -36,13 +7,15 @@ set_status() {
     local path='\[\e[0;34m\]\w\[\e[0m\]'
     local git_ref=$(git symbolic-ref HEAD 2> /dev/null) || return
 
+    PS1="\n"
+
     if [[  $UID == 0 ]] ; then
         local user='\[\e[0;31m\]\u\[\e[0m\]'
     else
         local user='\[\e[0;32m\]\u\[\e[0m\]'
     fi
 
-    PS1="$user at $host in $path"
+    PS1+="$user at $host in $path"
 
     if [[ -n $git_ref ]] ; then
         local git_dirty=$(git status --porcelain 2> /dev/null | tail -n1)
