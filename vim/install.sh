2ceb4c802bb45bb457df0acf83a261e2fcbf6a19
Per Kulseth Dahl <pkdahl@gmail.com>
2019-04-29 20:58:46 +0200

    Make Vim work better in Tmux

--- vim/install.sh
+++ vim/install.sh
@@ -19,18 +19,27 @@ fi
 
 VIM_PACK_DIR=$DOT_VIM_DIR/pack
 
+function install_pack() {
+	if [ ! -d $VIM_PACK_DIR/dist/start/$2 ]; then
+		echo_info "Installing $1"
+		mkdir -p $VIM_PACK_DIR/dist/start
+		git clone $1 $VIM_PACK_DIR/dist/start/$2
+		vim -u NONE -c "helptags $VIM_PACK_DIR/dist/start/$2/doc" -c q
+	fi
+}
+
 # if [ ! -d $DOT_VIM_DIR/pack/themes/opt/solarized8 ]; then
 #	echo_info "Installing solarized8"
 #	mkdir -p $DOT_VIM_DIR/pack/themes/opt
 #	git clone https://github.com/lifepillar/vim-solarized8.git $DOT_VIM_DIR/pack/themes/opt/solarized8
 # fi
 
-if [ ! -d $VIM_PACK_DIR/convenience/start/commentray ]; then
-	echo_info "Installing commentary"
-	mkdir -p $VIM_PACK_DIR/convenience/start
-	git clone https://github.com/tpope/vim-commentary.git \
-	          $VIM_PACK_DIR/convenience/start/commentary
-	vim -u NONE -c "helptags commentary/doc" -c q
-fi
+install_pack https://github.com/ctrlpvim/ctrlp.vim.git ctrlp
+install_pack https://github.com/tpope/vim-commentary.git commentary
+install_pack https://github.com/tpope/vim-fugitive fugitive
+install_pack https://github.com/masukomi/vim-markdown-folding.git markdown-folding
+install_pack https://github.com/vimwiki/vimwiki.git vimwiki
+install_pack https://github.com/tmux-plugins/vim-tmux.git tmux
+install_pack https://github.com/tmux-plugins/vim-tmux-focus-events.git tmux-focus-events
 
 echo_done "Setup vim"
