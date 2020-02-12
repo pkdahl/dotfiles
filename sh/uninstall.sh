7dc8aaf024a013f1b4db98d9bdbd69158261cc58
Per Kulseth Dahl <pkdahl@gmail.com>
2020-02-12 20:56:58 +0100

    Remove som un-/install scripts

--- sh/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo_info "Removing .profile"
-[ -L "$HOME/.profile" ] && rm -f "$HOME/.profile"
-
-echo_info "Removing .envir"
-[ -L "$HOME/.envir ] && rm -f "$HOME/.envir"
-
-echo_done "Removed sh setup"
