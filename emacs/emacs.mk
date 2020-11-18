c0ee1c3c1ba201fa13eaf807e2d3590cfc498ddd
Per Kulseth Dahl <pkdahl@gmail.com>
2020-11-18 16:33:44 +0100

    Currently don't have any abbrev_defs file

--- emacs/emacs.mk
+++ emacs/emacs.mk
@@ -4,14 +4,20 @@ EMACS_CONFIG_HOME := $(CONFIG_HOME)/emacs
 EMACS_VERSION := $(shell emacs --version | head -1 | sed 's/GNU Emacs //')
 
 $(EMACS_CONFIG_HOME)/init.el:
-	@echo $(EMACS_CONFIG_HOME)
 	mkdir -p $(@D)
 	ln -sf $(PWD)/init.el $@
+
+OO_DEPS := $(EMACS_CONFIG_HOME)/init.el
+
+# $(EMACS_CONFIG_HOME)/abbrev_defs:
+# 	mkdir -p $(@D)
+# 	ln -sf $(PWD)/abbrev_defs $@
+
+# OO_DEPS += $(EMACS_CONFIG_HOME)/abbrev_defs
 
 $(HOME)/.emacs.d:
 	ln -sf $(EMACS_CONFIG_HOME) $@
 
-OO_DEPS :=  $(EMACS_CONFIG_HOME)/init.el
 ifeq (1, $(shell echo "$(EMACS_VERSION) < 27" | bc))
 OO_DEPS += $(HOME)/.emacs.d
 endif
